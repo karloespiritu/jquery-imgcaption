1fbbe08e6f7987ee162f483d89a00a83851987a8
Karlo Espiritu <karloespiritu@gmail.com>
2014-05-30 01:42:32 +0800

    clean up code, updated minified version

--- jquery.imgcaption.js
+++ jquery.imgcaption.js
@@ -21,24 +21,23 @@
         return this.each(function() { 
             
             var $this = $(this); 
-                caption = $this.data('caption'), 
-                imgClass = $this.attr('class'),  
-                imgStyle = $this.attr('style'), 
+                caption = $this.data("caption"), 
+                imgClass = $this.attr("class"),  
+                imgStyle = $this.attr("style"), 
                 imgWidth = $this.width(); 
 
-            $this.removeAttr('class'); 
-            $this.removeAttr('style'); 
+            $this.removeAttr("class"); 
+            $this.removeAttr("style"); 
 
             if (caption) {
-                $figure = $this.wrap('<figure></figure>').parent(); 
-                $this.after('<figcaption>' + caption + '</figcaption>');
-                $figure
-                    .css({
+                $figure = $this.wrap("<figure></figure>").parent(); 
+                $this.after("<figcaption>" + caption + "</figcaption>");
+                
+                $figure.css({
                     "display" : "inline-block"})
                     .addClass(imgClass)
                     .attr("style", imgStyle)
-                    .width(imgWidth)
-                    .parent('p').before($figure); 
+                    .width(imgWidth);
 
                 $("figcaption").css({
                     "margin": "0px",
